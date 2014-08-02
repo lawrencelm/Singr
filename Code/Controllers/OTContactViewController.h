0cc3075551d57733970e4034adc1ffad8a896a67
duc <ducnguy@stanford.edu>
2014-08-02 12:42:50 -0700

    Pushing Contact View Controller

---
+++ Code/Controllers/OTContactViewController.h
@@ -0,0 +1,15 @@
+//
+//  OTContactViewController.h
+//  LayerSample
+//
+//  Created by Duc on 8/2/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTContactViewController : UITableViewController
+
+@property (nonatomic, weak) NSSet *contactList;
+
+@end
