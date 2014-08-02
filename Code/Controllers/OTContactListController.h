83be0e18aaab1b0971e35233a261586bc4332602
duc <ducnguy@stanford.edu>
2014-08-02 12:47:30 -0700

    adding contact view controllers

---
+++ Code/Controllers/OTContactListController.h
@@ -0,0 +1,13 @@
+//
+//  OTContactListController.h
+//  LayerSample
+//
+//  Created by Duc on 8/2/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTContactListController : UITableViewController
+@property (nonatomic, weak) *NSSet contactList;
+@end
